a4f8650ed78e60fd9f142955ea505cf8838aebc2
jiajun.chpu <jiajun.chou@104.com.tw>
2017-07-26 17:55:29 +0800

    Test randomNumer CI

---
+++ test/randomNum.test.js
@@ -0,0 +1,6 @@
+const randocall = require('../lib/randomNum')
+test('randocall calls its callback with a number', () => {
+  const mock = jest.fn();
+  randocall(mock);
+  expect(mock).toBeCalledWith(expect.any(Number));
+});
